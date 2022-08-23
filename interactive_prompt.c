1b5d000ffbaf8a3b17033c9fcb2df708370b6714
A Anandha Krishnan <anandha.krishnan@bigbinary.com>
2022-08-23 18:41:21 +0530

    add history and edit feature

--- interactive_prompt.c
+++ interactive_prompt.c
@@ -1,13 +1,20 @@
 #include<stdio.h>
-static char input[2048];
+#include<stdlib.h>
+
+#include<editline/readline.h>
+
 
 int main(int argc,char** argv){
   puts("Lispy version 0.0.0.0.1");
   puts("Press Ctrl+c to Exit\n");
   while(1){
-    fputs("lispy> ",stdout);
-    fgets(input,2048,stdin);
-    printf("No you're a %s",input);
+    char *input=readline("lispy> ");
+
+    add_history(input);
+
+    printf("No you're a %s\n",input);
+
+    free(input);
   }
   return 0;
 }
